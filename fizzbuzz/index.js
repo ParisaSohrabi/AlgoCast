11a4d3dd0ce98921387bc74bd476f0ca3a1e4346
Parisa <parisahs@gmail.com>
2022-12-18 15:54:39 -0500

    fizzbuzz

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -13,19 +13,24 @@
 //   buzz
 
 function fizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 15 === 0) {
-      console.log("fizzbuzz");
-    } else if (i % 3 === 0) {
-      console.log("fizz");
-    } else if (i % 5 === 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
-    }
+ for(let i=1;i<=n;i++){
+  if(i%15===0){
+    console.log("fizzbuzz")
   }
+  else if(i%5===0){
+    console.log('buzz')
+  }
+  else if(i%3===0){
+      console.log('fizz')}
+  
+else{
+  console.log(i)
+}
+  }
+
+ 
 }
 
-fizzBuzz(15);
-console.log(fizzBuzz(15));
+fizzBuzz(25);
+console.log(fizzBuzz(25));
 module.exports = fizzBuzz;
