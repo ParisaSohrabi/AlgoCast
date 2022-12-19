3a6a20ca9efd9ae9e8eb715a6f99caffae180efc
Parisa <parisahs@gmail.com>
2022-12-19 12:41:57 -0500

    anagram

--- anagrams/index.js
+++ anagrams/index.js
@@ -9,26 +9,25 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const acharMap = helper(stringA);
-  const bcharMap = helper(stringB);
-  if (Object.keys(acharMap).length !== Object.keys(bcharMap).length) {
-    return false;
-  }
-  for (let char in acharMap) {
-    if (acharMap[char] != bcharMap[char]) {
-      return false;
-    }
-    return true;
-  }
+ const astringA=clean(stringA)
+ const bstringB=clean(stringB)
+ if(Object.keys(astringA).length!== Object.keys(bstringB).length){
+   return false
+ }
+for(let char in astringA){
+   if(astringA[char]=== bstringB[char]){
+      return true
+   }
 }
-function helper(str) {
-  let charMap = {};
-  for (let char of str.replace(/[^\w]g/, "").toLowerCase()) {
-    charMap[char] = charMap[char]+ 1 || 1;
-  }
-  return charMap
 }
-
+function clean(str){
+   const charMap={}
+   const converted=str.replace(/[^\w]g/,'').toLowerCase().split('');
+for(let char of converted){
+charMap[char]= charMap[char]+1 || 1
+}
+return charMap
+}
 anagrams("rail safety", "fairy tales");
 console.log(anagrams("rail safety", "fairy tales"));
 module.exports = anagrams;
