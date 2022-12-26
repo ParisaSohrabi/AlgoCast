2a6b287a2ed8f6c89ec38bf787c8b75608fed6ce
Parisa <parisahs@gmail.com>
2022-12-26 18:28:24 -0500

    steps

--- steps/index.js
+++ steps/index.js
@@ -17,30 +17,24 @@
 //       '### '
 //       '####'
 
-// iterate through row s(from 0 to n)
-// create an empty string, "stair"
-// iterate through columns (from 0 to n)
-// if the current column is equal to or less than the current row
-// add a '#' to stair
-// else add a space to the 'stair'
-// console.log 'stair'
+//row=4 => 1st level: # -      =>row=0 col=0
+//         2nd level: # # -    =>row=1 col=0,col=1
+//         3rd level: # # # -  =>row=2 col=0,col=1,col=2
+//         4th level: # # # #  =>row=3 col=0,col=1,col=2,col=3
 
-// row=0 , col=0 => #
-// row=0 , col=1 => _
-// row=2 , col=2 => #
-// row=2 , col=3 => _
 function steps(n) {
-   for(let row=0;row<n;row++){
-    let level='';
+  for(let row=0;row<n;row++){
+    let step=''
     for(let col=0;col<n;col++){
         if(col<=row){
-            level+="#"
+           step+="#"
         }else{
-            level+=' '
+            step+=' '
         }
+
     }
-    console.log(level)
-   }
+    console.log(step)
+  } 
 }
 steps(4)
 console.log(steps(4))
