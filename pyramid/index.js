14a1fed7ca211fad670beaacd6295021b21c5b9c
Parisa <parisahs@gmail.com>
2022-12-28 12:40:03 -0500

    pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,20 +14,28 @@
 //       ' ### '
 //       '#####'
 
-// pyramid(4)=> 4 rows and 7column
+// pyramid(4) ---#--- row=0, col=3
+//            --###-- row=1,col=2,3,4
+//            -#####- row=2,col=1,2,3,4,5
+//            ####### row=3,col=0,1,2,3,4,5,6
+// row start from 0 to N
+// make a new level
+// col start from 0 to N*2-1
+// find out the middlepoint
+// print the #
+
 function pyramid(n) {
-  let middlePoint = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let level = "";
-    for (let col = 0; col < 2 * n - 1; col++) {
-      if (middlePoint - row <= col && middlePoint + row >= col) {
-        level += "#";
-      } else {
-        level += " ";
+  const middlepoint= Math.floor((n*2-1)/2)
+  for(let row=0;row<n;row++){
+    let level=''
+    for(let col=0;col<n*2-1;col++){
+      if(middlepoint-row<=col && middlepoint+row>=col){
+        level+='#'
+      }else{
+        level+=' '
       }
     }
-  console.log(level)
-
+    console.log(level)
   }
 }
 
