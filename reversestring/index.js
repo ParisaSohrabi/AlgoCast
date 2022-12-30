0a41ee834db67aa664d127f75fd4f007b2e5f363
Parisa <parisahs@gmail.com>
2022-12-30 12:02:04 -0500

    reversedString- chunk

--- reversestring/index.js
+++ reversestring/index.js
@@ -7,13 +7,12 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
- let reversed='';
- for(let char of str){
-  reversed= char+reversed;
- }
- return reversed;
+let reversed='';                    
+for(let char of str){
+reversed= char+reversed
+}
+return reversed
 }
 
 module.exports = reverse;
 
-// return str.split('').reverse().join('')
