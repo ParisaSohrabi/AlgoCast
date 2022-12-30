f152178b9cf5510d1c0292b23950b42942bc7996
Parisa <parisahs@gmail.com>
2022-12-30 12:10:39 -0500

    reverse integer

--- reverseint/index.js
+++ reverseint/index.js
@@ -9,8 +9,9 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-    const num=n.toString().split('').reverse().join('');
-    return parseInt(num)*Math.sign(n)
+  let reversed="";
+  reversed= n.toString().split('').reverse().join('')
+  return parseInt(reversed)*Math.sign(n)
 }
 reverseInt(-43)
 console.log(reverseInt(82))
