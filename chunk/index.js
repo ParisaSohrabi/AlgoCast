a8ed61c69af63393f86d5e60cc755bfa0ba9df99
Parisa <parisahs@gmail.com>
2023-03-07 12:53:12 -0500

    chunk

--- chunk/index.js
+++ chunk/index.js
@@ -9,13 +9,13 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-const chunked=[]
+let result=[]
 let index=0
 while(index<array.length){
-  chunked.push(array.slice(index,index+size))
+  result.push(array.slice(index,index+size))
   index+=size
 }
-return chunked
+return result
 }
 chunk([1, 2, 3, 4, 5], 2)
 console.log( chunk([1, 2, 3, 4, 5], 2))
