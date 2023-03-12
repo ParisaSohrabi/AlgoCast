ce8f28ee0f0261c962cdd6466510744a2f50faa6
Parisa <parisahs@gmail.com>
2023-03-12 14:11:14 -0400

    pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -18,25 +18,21 @@
 //            --###-- row=1,col=2,3,4
 //            -#####- row=2,col=1,2,3,4,5
 //            ####### row=3,col=0,1,2,3,4,5,6
-// row start from 0 to N
-// make a new level
-// col start from 0 to N*2-1
-// find out the middlepoint
-// print the #
+
 
 function pyramid(n) {
-  const middlepoint= Math.floor((n*2-1)/2)
   for(let row=0;row<n;row++){
-    let level=''
-    for(let col=0;col<n*2-1;col++){
-      if(middlepoint-row<=col && middlepoint+row>=col){
-        level+='#'
+    let step=''
+    const middle=Math.floor((2*n-1)/2)
+    for(let col=0;col<(2*n-1);col++){
+      if(middle-row<=col && middle+row>=col){
+        step+='#'
       }else{
-        level+=' '
+        step+=' '
       }
     }
-    console.log(level)
+    console.log(step)
   }
 }
-
+pyramid(4)
 module.exports = pyramid;
