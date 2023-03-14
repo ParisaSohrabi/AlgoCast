0966cb48d711b1939203d3728a9d5e716924ed20
Parisa <parisahs@gmail.com>
2023-03-14 13:40:20 -0400

    fib

--- fib/index.js
+++ fib/index.js
@@ -10,14 +10,15 @@
 
 
 function fib(n) {
- result=[0,1]
- for(let i=2;i<=n;i++){
-const a=result[i-1]
-const b=result[i-2]
-result.push(a+b)
+ let result=[0,1]
+ for(let i=2;i<n;i++){
+    const a=result[i-1]
+    const b=result[i-2]
+    result.push(a+b)
  }
- return result[n]
+ return result
 }
-
+fib(6)
+console.log( fib(6))
 module.exports = fib;
 
